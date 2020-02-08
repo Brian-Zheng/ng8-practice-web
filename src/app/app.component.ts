b6c475ef536384a9a3466e5896e89fe9e2d85303
200067 <brianzheng@systex.com>
2020-02-08 23:41:00 +0800

    路由設定  簡易驗證

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Constant
+import { appPath } from './const/app-path.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-practice-web';
+  path = appPath;
 }
