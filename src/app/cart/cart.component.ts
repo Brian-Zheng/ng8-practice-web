5ccc9055a870ca3c639675148084a1ef7f563d1b
1500349 <brianzheng@systex.com>
2020-02-11 21:56:48 +0800

    ngFor   ngClass

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,6 +11,27 @@ export class CartComponent implements OnInit {
 
   path = appPath;
 
+  commodityList = [
+    {
+      title: "草莓馬卡龍",
+      dollarSign: "NT$",
+      dollar: 250,
+      img: "/assets/images/others/brenda-godinez-367708-unsplash.jpg"
+    },
+    {
+      title: "焦糖馬卡龍",
+      dollarSign: "NT$",
+      dollar: 200,
+      img: "/assets/images/others/food-photographer-jennifer-pallian-650641-unsplash.jpg"
+    },
+    {
+      title: "某種甜點?",
+      dollarSign: "NT$",
+      dollar: "???",
+      img: "/assets/images/others/brooke-lark-96398-unsplash.jpg"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
